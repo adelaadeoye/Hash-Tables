e2e13a160dda5ca2c8b8c742a90f1c6c34404ed4
adeoye adela <adeoye.adela@gmail.com>
2020-05-07 23:46:42 +0100

    Expensive sequnce done

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,6 +1,12 @@
+hashTable={}
 def expensive_seq(x, y, z):
     # Implement me
-
+    key=(x,y,z) 
+    if x <= 0:
+        return y + z
+    if key  not in hashTable: 
+        hashTable[key]= expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+    return hashTable[key]
 if __name__ == "__main__":
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
