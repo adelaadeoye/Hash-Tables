8744e6aab30ff3a7cbf818528f24d9058480f4f5
adeoye adela <adeoye.adela@gmail.com>
2020-05-08 01:44:48 +0100

    word count working

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,9 +1,22 @@
+import re
+
 def word_count(s):
+   cache={}
     # Implement me.
-
-
+   split= s.split()
+   for item in split:
+       key=''.join( c for c in item if  c not in ':;,.-+=/\\|[]{}()*^&"' )
+       key=key.lower()
+       if key =="":
+           return {}
+       elif key not in cache :
+           cache[key]=1
+       else:
+            cache[key]+=1
+   return (cache)
 if __name__ == "__main__":
     print(word_count(""))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
